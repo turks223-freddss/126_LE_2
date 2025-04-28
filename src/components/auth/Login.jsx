ae887f6b166326ff65b8ce24309b17d492252330
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-04-28 20:10:42 +0800

    LogIn Front-End Version 1

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -26,61 +26,81 @@ const Login = () => {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Sign in to your account
-                    </h2>
-                </div>
-                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    {error && (
-                        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                            <span className="block sm:inline">{error}</span>
+        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-100 p-6">
+            <div className="text-center mb-6">
+                <h1 className="text-4xl text-gray-900 font-extrabold">Budget Tracker</h1>
+                <hr className="border-t-2 border-black  mx-auto w-full" />
+            </div>
+            <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-lg border border-gray-300">
+                <h2 className="text-2xl text-center font-semibold text-gray-900 mb-4">Sign in to your account</h2>
+
+                {error && (
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
+                        <span>{error}</span>
+                    </div>
+                )}
+
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <div className='space-y-4'>
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    className="appearance-none block w-full px-4 py-2 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-gray-900"
+                                    placeholder="Email address"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    className="appearance-none block w-full px-4 py-2 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-gray-900"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
                         </div>
-                    )}
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label htmlFor="email" className="sr-only">Email address</label>
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Email address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                        <div className="text-right">
+                            <a href="/password-reset" className="text-sm text-indigo-600 hover:text-indigo-900 font-semibold">
+                                Forgot password?
+                            </a>
                         </div>
-                        <div>
-                            <label htmlFor="password" className="sr-only">Password</label>
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
+
                     </div>
 
-                    <div>
+                    <div className="flex justify-center mt-6">
                         <button
                             type="submit"
                             disabled={loading}
-                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="w-1/4 py-2 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:scale-105 hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-900 focus:ring-offset-2 disabled:bg-indigo-300 transition-transform duration-200 ease-in-out"
                         >
                             {loading ? 'Signing in...' : 'Sign in'}
                         </button>
                     </div>
+
+                    <div className="mt-4 text-center">
+                        <p className="text-sm text-gray-600">
+                            Don't have an account?{' '}
+                            <a href="/register" className="text-indigo-600 hover:text-indigo-900 font-semibold">
+                                Sign up here
+                            </a>
+                        </p>
+                    </div>
+                    
                 </form>
             </div>
         </div>
     );
 };
 
-export default Login; 
+export default Login;

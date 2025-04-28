87397a4be000c0c03c5a0195352cf14bb0a6c33a
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-04-28 20:56:42 +0800

    Password Reset FrontEnd

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -59,7 +59,7 @@ const Register = () => {
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Create your account
+                        Create your accountssssss 
                     </h2>
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>

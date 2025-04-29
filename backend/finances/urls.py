0c05dd68d0a3a5e6f7ca1f3be3f9c2c0f752d88c
turks223-freddss <marfred.deen02@gmail.com>
2025-04-29 22:38:38 +0800

    Testing

--- backend/finances/urls.py
+++ backend/finances/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import AddIncomeView, AddExpenseView, BudgetCalculatorView, finance_details
+from .views import AddIncomeView, AddExpenseView, BudgetCalculatorView, finance_details, list_user_income, list_user_expense, modify_income,modify_expense
 
 urlpatterns = [
     path('add-income/', AddIncomeView.as_view(), name='add-income'),
     path('add-expense/', AddExpenseView.as_view(), name='add-expense'),
     path('budget/', BudgetCalculatorView.as_view(), name='budget'),
     path('<int:user_id>/finance-details/', finance_details, name='finance_details'),
+    path('list-income/', list_user_income, name='list_user_income'),
+    path('list-expense/', list_user_expense, name='list_user_expense'),
+    path('income/<int:income_id>/', modify_income, name='modify_income'),
+    path('expense/<int:expense_id>/', modify_expense, name='modify_expense'),
 ]

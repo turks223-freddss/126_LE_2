2cc698abac797991251580427f6e6732ac50175c
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-04-30 14:56:27 +0800

    small backend update

--- backend/finances/urls.py
+++ backend/finances/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddIncomeView, AddExpenseView, BudgetCalculatorView, finance_details, list_user_income, list_user_expense, modify_income,modify_expense, delete_finance_entry, update_finance_entry
+from .views import AddIncomeView, AddExpenseView, BudgetCalculatorView, finance_details, delete_finance_entry, update_finance_entry
 from . import views
 
 urlpatterns = [
@@ -9,13 +9,5 @@ urlpatterns = [
     path('<int:user_id>/finance-details/', finance_details, name='finance_details'),
     path('delete/<int:user_id>/<int:entry_id>/', delete_finance_entry, name='delete_finance_entry'),
     path('update/<int:user_id>/<int:entry_id>/', update_finance_entry, name='update_finance_entry'),
-    
-    # not used
-    path('list-income/', list_user_income, name='list_user_income'),
-    path('list-expense/', list_user_expense, name='list_user_expense'),
-    path('income/<int:income_id>/', modify_income, name='modify_income'),
-    path('expense/<int:expense_id>/', modify_expense, name='modify_expense'),
-    
-    
     path('<int:user_id>/reports/', views.get_reports, name='get_reports'),
 ]
